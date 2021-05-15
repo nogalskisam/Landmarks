7a8460af15967e29aac1cce3b1e7fe032a5c84e0
Samuel Nogalski <sam.nogalski@nhs.net>
2021-05-15 18:57:08 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sam Nogalski on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
